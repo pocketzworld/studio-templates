60309557166a8888c918b8658c1f0b11cb873426
erinmsp <162064720+erinmsp@users.noreply.github.com>
2025-11-03 17:23:45 -0600

    iso template

--- Iso_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/ISOCamera.cs
+++ Iso_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/ISOCamera.cs
@@ -15,6 +15,7 @@ using UnityEngine;
 using Highrise.Client;
 using Highrise.Studio;
 using Highrise.Lua;
+using UnityEditor;
 
 namespace Highrise.Lua.Generated
 {
@@ -55,6 +56,14 @@ namespace Highrise.Lua.Generated
                 CreateSerializedProperty(_script.GetPropertyAt(8), m_keepPlayerInView),
             };
         }
+        
+#if HR_STUDIO
+        [MenuItem("CONTEXT/ISOCamera/Edit Script")]
+        private static void EditScript()
+        {
+            VisualStudioCodeOpener.OpenPath(AssetDatabase.GUIDToAssetPath(s_scriptGUID));
+        }
+#endif
     }
 }
 
