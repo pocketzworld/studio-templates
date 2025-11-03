b1a4a7dc3947d49ad97c2dd74c6af612c5973b08
erinmsp <162064720+erinmsp@users.noreply.github.com>
2025-11-03 17:35:07 -0600

    job sim template

--- JobSim_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/CreateOrderGui.cs
+++ JobSim_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/CreateOrderGui.cs
@@ -15,6 +15,7 @@ using UnityEngine;
 using Highrise.Client;
 using Highrise.Studio;
 using Highrise.Lua;
+using UnityEditor;
 
 namespace Highrise.Lua.Generated
 {
@@ -38,6 +39,14 @@ namespace Highrise.Lua.Generated
                 CreateSerializedProperty(_script.GetPropertyAt(2), null),
             };
         }
+        
+#if HR_STUDIO
+        [MenuItem("CONTEXT/CreateOrderGui/Edit Script")]
+        private static void EditScript()
+        {
+            VisualStudioCodeOpener.OpenPath(AssetDatabase.GUIDToAssetPath(s_scriptGUID));
+        }
+#endif
     }
 }
 
