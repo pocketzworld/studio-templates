272e6b8804fe4e705fb6f6322d6b77811b17dce6
erinmsp <162064720+erinmsp@users.noreply.github.com>
2025-11-03 17:43:52 -0600

    joystick template

--- Joystick_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/SideScrollerCamera.cs
+++ Joystick_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/SideScrollerCamera.cs
@@ -15,6 +15,7 @@ using UnityEngine;
 using Highrise.Client;
 using Highrise.Studio;
 using Highrise.Lua;
+using UnityEditor;
 
 namespace Highrise.Lua.Generated
 {
@@ -72,6 +73,14 @@ namespace Highrise.Lua.Generated
                 CreateSerializedProperty(_script.GetPropertyAt(17), m_deadzoneHeight),
             };
         }
+        
+#if HR_STUDIO
+        [MenuItem("CONTEXT/SideScrollerCamera/Edit Script")]
+        private static void EditScript()
+        {
+            VisualStudioCodeOpener.OpenPath(AssetDatabase.GUIDToAssetPath(s_scriptGUID));
+        }
+#endif
     }
 }
 
