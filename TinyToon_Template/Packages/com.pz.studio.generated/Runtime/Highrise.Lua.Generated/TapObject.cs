6be672c3fc8d8be2aa6a87950163cd2ba7f23868
erinmsp <162064720+erinmsp@users.noreply.github.com>
2025-11-04 10:05:01 -0600

    tiny toon template

--- TinyToon_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/TapObject.cs
+++ TinyToon_Template/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/TapObject.cs
@@ -15,6 +15,7 @@ using UnityEngine;
 using Highrise.Client;
 using Highrise.Studio;
 using Highrise.Lua;
+using UnityEditor;
 
 namespace Highrise.Lua.Generated
 {
@@ -39,6 +40,14 @@ namespace Highrise.Lua.Generated
                 CreateSerializedProperty(_script.GetPropertyAt(1), _tapSound),
             };
         }
+        
+#if HR_STUDIO
+        [MenuItem("CONTEXT/TapObject/Edit Script")]
+        private static void EditScript()
+        {
+            VisualStudioCodeOpener.OpenPath(AssetDatabase.GUIDToAssetPath(s_scriptGUID));
+        }
+#endif
     }
 }
 
